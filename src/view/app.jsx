fe98c762448723ebe9f9b6f4caa54d0bab0ccfac
Dmytro Karpov <dimon.karpov1@gmail.com>
2018-07-19 16:01:31 +0200

    wall works

--- src/view/app.jsx
+++ src/view/app.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Login from './loginform.jsx';
 import './wall.less';
 
+const urlPath = "https://jsonplaceholder.typicode.com/posts";
+
 class App extends Component {
     constructor(props) {
         super(props);
@@ -16,8 +18,7 @@ class App extends Component {
     validateForm = (login, pass) => {
         const strongPass = new RegExp(this.props.regExpValidator);
 
-        if (strongPass.test(pass) && (login.length < this.props.minPassLength)) {
-            console.log(pass);
+        if (/*strongPass.test(pass) && */(login.length > this.props.minPassLength)) {
             this.setState({ invalidLogin: false });
             this.setState({ loggedIn: true });
         }
@@ -26,21 +27,66 @@ class App extends Component {
     }
 
     componentDidMount() {
-        fetch('https://api.mydomain.com')
-            .then(response => response.json())
-            .then(data => this.setState({ posts: JSON.parse(data) }));
-
-            console.log(this.state.posts);
+        const request = new XMLHttpRequest();
+        request.addEventListener("load", this.transferComplete);
+        request.addEventListener("error", this.transferFailed);
+        request.open("GET", urlPath);
+        request.send();
     }
 
+    transferComplete = (evt) => {
+        console.log("The transfer is complete.");
+        this.setState({ posts: evt.target.response });
+    }
+
+    transferFailed = (evt) => {
+        console.log("An error occurred while transferring the file.", evt);
+        this.setState({ error: evt.type });
+    }
 
     render() {
+        let domElement = null;
+
+        if (this.state.error) {
+            // potentially place for updating UI
+            window.alert(this.state.error);
+        }
+
+        if (this.state.loggedIn && this.state.posts.length) {
+            console.log(this.state.posts)
+            const posts = Array.from(this.state.posts).map((post) =>
+                <Post key={post.id} id={post.id} title={post.title} body={post.body} />
+            );
+
+            domElement =
+                <div>
+                    <input className='search' placeholder="search posts" />
+                    <div className='flex-container'>
+                        {posts}
+                    </div>
+                </div>
+        }
+        else
+            domElement = <Login validationCallback={this.validateForm} warning={this.state.invalidLogin} />
+
         return (
-            <div>
-                <Login validationCallback={this.validateForm} warning={this.state.invalidLogin} />
-            </div>
+            <div>{domElement}</div>
         );
     }
 }
 
+App.defaultProps = {
+    minPassLength: 5,
+    regExpValidator: "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})"
+};
+
 export default App
+
+function Post({id, title, body}) {
+    
+    return (
+        <div className='flex-item'>
+            TEST POST
+        </div>
+    );
+}
