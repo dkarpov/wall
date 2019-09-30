ab02574a825e94d415fabe97764ec739d7784610
Dmytro Karpov <dimon.karpov1@gmail.com>
2019-09-30 17:45:44 +0200

    gh-page integration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require("path");
 
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
@@ -8,30 +8,30 @@ const htmlWebpackPlugin = new HtmlWebPackPlugin({
 });
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
-    },
-    devtool: 'inline-source-map',
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        },
-        {
-          test: /\.(css|less)$/,
-          use: ["style-loader", "css-loader", "less-loader"]
-        },
-      ]
-    },
-    devServer: {
-      historyApiFallback: true,
-    },
-    plugins: [htmlWebpackPlugin]
-  };
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/"
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(css|less)$/,
+        use: ["style-loader", "css-loader", "less-loader"]
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [htmlWebpackPlugin]
+};
